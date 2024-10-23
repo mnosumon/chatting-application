2e7cde26751c254dbf4a543277f7da6c817e3da6
mnosumon <mdnuruddin378350@gmail.com>
2024-10-23 12:57:24 +0600

    Friends componets used message page

--- src/components/home/Friends.jsx
+++ src/components/home/Friends.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Friends = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
   return (
     <div className="mt-5">
       <div className="bg-[#FBFBFB] px-4 pt-8 border shadow-md rounded-md">
@@ -12,9 +15,18 @@ const Friends = () => {
               <h4 className="text-lg font-sans">Md Helal</h4>
             </div>
             <div>
-              <button className="text-base font-sans py-1 px-3 bg-[#4A81D3] text-white rounded-md">
-                Message
-              </button>
+              {location.pathname == "/" ? (
+                <button
+                  onClick={() => {
+                    navigate("/message");
+                  }}
+                  className="text-base font-sans py-1 px-3 bg-[#4A81D3] text-white rounded-md"
+                >
+                  Message
+                </button>
+              ) : (
+                ""
+              )}
             </div>
           </div>
           <div className="flex justify-between items-center mb-3">
@@ -23,9 +35,18 @@ const Friends = () => {
               <h4 className="text-lg font-sans">Md Helal</h4>
             </div>
             <div>
-              <button className="text-base font-sans py-1 px-3 bg-[#4A81D3] text-white rounded-md">
-                Message
-              </button>
+              {location.pathname == "/" ? (
+                <button
+                  onClick={() => {
+                    navigate("/message");
+                  }}
+                  className="text-base font-sans py-1 px-3 bg-[#4A81D3] text-white rounded-md"
+                >
+                  Message
+                </button>
+              ) : (
+                ""
+              )}
             </div>
           </div>
           <div className="flex justify-between items-center mb-3">
@@ -34,31 +55,18 @@ const Friends = () => {
               <h4 className="text-lg font-sans">Md Helal</h4>
             </div>
             <div>
-              <button className="text-base font-sans py-1 px-3 bg-[#4A81D3] text-white rounded-md">
-                Message
-              </button>
-            </div>
-          </div>
-          <div className="flex justify-between items-center mb-3">
-            <div className="flex gap-x-2 items-center">
-              <div className="w-10 h-10 rounded-full bg-sky-400"></div>
-              <h4 className="text-lg font-sans">Md Helal</h4>
-            </div>
-            <div>
-              <button className="text-base font-sans py-1 px-3 bg-[#4A81D3] text-white rounded-md">
-                Message
-              </button>
-            </div>
-          </div>
-          <div className="flex justify-between items-center mb-3">
-            <div className="flex gap-x-2 items-center">
-              <div className="w-10 h-10 rounded-full bg-sky-400"></div>
-              <h4 className="text-lg font-sans">Md Helal</h4>
-            </div>
-            <div>
-              <button className="text-base font-sans py-1 px-3 bg-[#4A81D3] text-white rounded-md">
-                Message
-              </button>
+              {location.pathname == "/" ? (
+                <button
+                  onClick={() => {
+                    navigate("/message");
+                  }}
+                  className="text-base font-sans py-1 px-3 bg-[#4A81D3] text-white rounded-md"
+                >
+                  Message
+                </button>
+              ) : (
+                ""
+              )}
             </div>
           </div>
         </div>
--- src/pages/message/Message.jsx
+++ src/pages/message/Message.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Friends from "../../components/home/Friends";
 
 const Message = () => {
-  return <div>Message</div>;
+  return (
+    <div className="w-full">
+      <div className="grid grid-cols-[1fr,3fr] gap-x-12">
+        <div className="">
+          <Friends />
+        </div>
+        <div className=""></div>
+      </div>
+    </div>
+  );
 };
 
 export default Message;
